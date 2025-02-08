b91934a96eda2fb14c94f0b73d6d74e708873ebb
aditya srivastava <adityasrvastava2004@gmail.com>
2025-02-08 21:36:21 +0530

    Commit Section Done React

--- forntEnd/src/Pages/student/Home.jsx
+++ forntEnd/src/Pages/student/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Hero from '../../components/student/Hero'
+import Companies from '../../components/student/Companies'
+import CourseCard from '../../components/student/CourseCard'
+import CoursesSecton from '../../components/student/CoursesSecton'
 
 const Home = () => {
   return (
-    <div className='flex felx-col items-center space-y-7 text-center'>
+    <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero/>
+      <Companies/>
+      <CoursesSecton/>
     </div>
   )
 }
--- forntEnd/src/components/student/CoursesSecton.jsx
+++ forntEnd/src/components/student/CoursesSecton.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const CoursesSecton = () => {
   return (
     <div>
-      <h1>CoursesSecton</h1>
+      <div className='py-16 md:px-40 px-8'>
+      <h2 className='text-3xl font-medium text-gray-800 ' >Learn from the best</h2>
+      <p className='md:text-base text-sm text-gray-500 mt-3 pb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. .</p>
+      <Link to={'/course-list'} onClick={()=>scrollTo(0,0)} className='text-gray-500 border border-gray-500/30 px-10 py-3  rounded'>Show All Courses</Link>
+    </div>
     </div>
   )
 }
