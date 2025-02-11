7945c9f78fe45ea8e196a79a3db8ac30de3d8c31
aditya srivastava <adityasrvastava2004@gmail.com>
2025-02-11 19:06:56 +0530

    Home Page Done React

--- forntEnd/src/Pages/student/Home.jsx
+++ forntEnd/src/Pages/student/Home.jsx
@@ -3,6 +3,8 @@ import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CoursesSecton from '../../components/student/CoursesSecton'
 import Testimonials from '../../components/student/testimonial' 
+import CallToAction from '../../components/student/CallToAction'
+import Footer from '../../components/student/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Companies/>
       <CoursesSecton/>
       <Testimonials/>
+      <CallToAction/>
+      <Footer/>
     </div>
   )
 }
--- forntEnd/src/components/student/testimonial.jsx
+++ forntEnd/src/components/student/testimonial.jsx
@@ -6,7 +6,7 @@ const Testimonials = () => {
     <div className='pb-14 px-8 md:px-0'>
       <h2 className='text-3xl font-medium  text-gray-800'>Testimonials</h2>
       <p className='md:text-base text-gray-500 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Assumenda placeat quo, praesentium temporibus doloremque hic fugiat tempora unde provident optio?</p>
-      <div>
+      <div className='grid grid-cols-3 gap-8 m-14'>
         {dummyTestimonial.map((testimonial,index)=>(
           <div className='text-sm text-left border border-gray-500/30 pb-6 rounded-lg bg-white shadow-[0px_4px_15px_0px] shadow-black/5 overflow-hidden' key={index}>
             <div className='flex items-center gap-4 px-5 py-4 bg-gray-500/10'>
@@ -15,15 +15,16 @@ const Testimonials = () => {
                 <h1 className='text-lg font-medium text-gray-800'>{testimonial.name}</h1>
                 <p className='text-gray-800/80'>{testimonial.role}</p>
               </div>
+              </div>
               <div className='p-5 pb -7'>
                 <div className='flex gap=0.5'>
-                  {[...Array(5).map((_,i)=>(
+                  {[...Array(5)].map((_,i)=>(
                   <img key={i}src={i<Math.floor(testimonial.rating)?assets.star:assets.star_blank} alt='star' className='h-5'/>
-                  ))]}
+                  ))}
                 </div>
                 <p>{testimonial.feedback}</p>
               </div>
-              </div>
+              <a href='#' className=' text-blue-500 underline px-5'>Read more</a>
           </div>
         ))}
       </div>
